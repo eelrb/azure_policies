5bff4ccc39c74faaf901def8f343971d81e461d7
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-20 11:14:45 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
-# Configure the Microsoft Azure Provider.
+# Configure the Azure Provider
 provider "azurerm" {
-    version = "=1.44.0"
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=1.44.0"
 }
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "production"
+  location = "West US"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "production-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
 
 data "template_file" "requiredTag_policy_rule" {
   template = <<POLICY_RULE
