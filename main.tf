0424b883cca82a9962e75750e436b784266278f8
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-20 11:06:55 -0800

    diff ex

--- main.tf
+++ main.tf
@@ -3,67 +3,40 @@ provider "azurerm" {
     version = "=1.30.0"
 }
 
-# Define Azure Policy Definition
-resource "azurerm_policy_definition" "blee-policy" {
-  name         = "PaC-Naming-Convention"
-  policy_type  = "Custom"
-  mode         = "Indexed"
-  display_name = "PaC_Naming_Convention"
-
-  metadata     = <<METADATA
-    {
-    "category": "Demo"
-    }
-  METADATA
-
-  policy_rule = <<POLICY_RULE
-    {
-    "if": {
-		"allOf":[
-			{
-				"not":{
-					"field":"name",
-					"match":"[parameters('namePattern')]"
-				}
-			},
-			{
-				"field": "type",
-				"equals": "Microsoft.Compute/virtualMachines"
-			}
-		]
-    },
-    "then": { 
-      "effect": "deny"
-    }
-  }
-POLICY_RULE
-
-  parameters = <<PARAMETERS
-    {
-		"namePattern":{
-			"type": "String",
-			"metadata":{
-				"displayName": "namePattern",
-				"description": "? for letter, # for numbers"
-			}
-		}
-  }
-PARAMETERS
-}
-
-# Define Azure Policy Assignment
-resource "azurerm_policy_assignment" "blee-policy-assignment" {
-  name                 = "Naming-Convention-Assignment"
-  scope                = "/subscriptions/00000001-0001-0001-0001-000000000001"
-  policy_definition_id = "${azurerm_policy_definition.policy.id}"
-  description          = "Naming convention for VM"
-  display_name         = "Naming-Convention-Assignment"
-
-  parameters = <<PARAMETERS
+data "template_file" "requiredTag_policy_rule" {
+  template = <<POLICY_RULE
 {
-  "namePattern": {
-    "value": "demo-???-####"
-  }
+    "if": {
+        "field": "[concat('tags[', parameters('tagName'), ']')]",
+        "exists": "false"
+    },
+    "then": {
+        "effect": "audit"
+    }
+}
+POLICY_RULE
+}
+
+data "template_file" "requiredTag_policy_parameters" {
+  template = <<PARAMETERS
+{
+    "tagName": {
+        "type": "String",
+        "metadata": {
+            "displayName": "Tag Name",
+            "description": "Name of the tag, such as 'environment'"
+        }
+    }
 }
 PARAMETERS
+}
+
+resource "azurerm_policy_definition" "requiredTag" {
+  name         = "Audit-RequiredTag-Resource"
+  display_name = "Audit a Required Tag on a Resource"
+  description  = "Audit all resources for a required tag"
+  policy_type  = "Custom"
+  mode         = "All"
+  policy_rule  = "${data.template_file.requiredTag_policy_rule.rendered}"
+  parameters   = "${data.template_file.requiredTag_policy_parameters.rendered}"
 }
