d96dfb7ff16ac6154ff192d2e930f4883ed5a922
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-20 10:40:26 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,52 @@
+# Configure the Microsoft Azure Provider.
+provider "azurerm" {
+    version = "=1.30.0"
+}
+
+# Define Azure Policy Definition
+resource "azurerm_policy_definition" "policy" {
+  name         = "PaC-Naming-Convention"
+  policy_type  = "Custom"
+  mode         = "Indexed"
+  display_name = "PaC_Naming_Convention"
+
+  metadata     = <<METADATA
+    {
+    "category": "Demo"
+    }
+  METADATA
+
+  policy_rule = <<POLICY_RULE
+    {
+    "if": {
+		"allOf":[
+			{
+				"not":{
+					"field":"name",
+					"match":"[parameters('namePattern')]"
+				}
+			},
+			{
+				"field": "type",
+				"equals": "Microsoft.Compute/virtualMachines"
+			}
+		]
+    },
+    "then": { 
+      "effect": "deny"
+    }
+  }
+POLICY_RULE
+
+  parameters = <<PARAMETERS
+    {
+		"namePattern":{
+			"type": "String",
+			"metadata":{
+				"displayName": "namePattern",
+				"description": "? for letter, # for numbers"
+			}
+		}
+  }
+PARAMETERS
+}
