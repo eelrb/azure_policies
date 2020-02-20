6c44424a7886de29745ae8c2fb690d048fed5fff
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-20 14:02:19 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "policy_1_id" {
   value = azurerm_policy_definition.blee-policy-1.id
 }
+
+output "policy_2_id" {
+  value = azurerm_policy_definition.blee-policy-2.id
+}
+
+output "policy_3_id" {
+  value = azurerm_policy_definition.blee-policy-3.id
+}
