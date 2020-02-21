e3a9d0be3ee78feb26bcb5d68d0e0a86e1480745
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-21 05:49:24 -0800

    Update policy3.tf

--- policy3.tf
+++ policy3.tf
@@ -1,1 +1,10 @@
 
+resource "azurerm_policy_definition" "blee-policy-3" {
+  name         = "blee-policy-3"
+  display_name = "Policy-3"
+  description  = "Policy-3-Description"
+  policy_type  = "Custom"
+  mode         = "All"
+  policy_rule  = "${data.template_file.requiredTag_policy_rule.rendered}"
+  parameters   = "${data.template_file.requiredTag_policy_parameters.rendered}"
+}
