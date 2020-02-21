141fee28b9bfccd0e5e38b0975be774102543e33
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-21 05:48:05 -0800

    Update policy2.tf

--- policy2.tf
+++ policy2.tf
@@ -1,1 +1,10 @@
 
+resource "azurerm_policy_definition" "blee-policy-2" {
+  name         = "blee-policy-2"
+  display_name = "Audit a Required Tag on a Resource"
+  description  = "Audit all resources for a required tag"
+  policy_type  = "Custom"
+  mode         = "All"
+  policy_rule  = "${data.template_file.requiredTag_policy_rule.rendered}"
+  parameters   = "${data.template_file.requiredTag_policy_parameters.rendered}"
+}
