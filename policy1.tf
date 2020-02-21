f447940f6fa738a2d68036cf61f5b1d136517667
Brian Lee <35783300+eelrb@users.noreply.github.com>
2020-02-20 16:42:01 -0800

    Update policy1.tf

--- policy1.tf
+++ policy1.tf
@@ -1,1 +1,42 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=1.44.0"
+}
 
+data "template_file" "requiredTag_policy_rule" {
+  template = <<POLICY_RULE
+{
+    "if": {
+        "field": "[concat('tags[', parameters('tagName'), ']')]",
+        "exists": "true"
+    },
+    "then": {
+        "effect": "disabled"
+    }
+}
+POLICY_RULE
+}
+
+data "template_file" "requiredTag_policy_parameters" {
+  template = <<PARAMETERS
+{
+    "tagName": {
+        "type": "String",
+        "metadata": {
+            "displayName": "Tag Name",
+            "description": "Name of the tag, such as 'environment'"
+        }
+    }
+}
+PARAMETERS
+}
+
+resource "azurerm_policy_definition" "blee-policy-1" {
+  name         = "blee-policy-1"
+  display_name = "Audit a Required Tag on a Resource"
+  description  = "Audit all resources for a required tag"
+  policy_type  = "Custom"
+  mode         = "All"
+  policy_rule  = "${data.template_file.requiredTag_policy_rule.rendered}"
+  parameters   = "${data.template_file.requiredTag_policy_parameters.rendered}"
+}
